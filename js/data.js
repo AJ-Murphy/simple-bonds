043b1f8899f06f0fe7bce58781bb46da5b27de7f
Adam <aj.murphy@outlook.com>
2019-10-31 16:28:11 +0000

    API now pulled from my-json-server

--- js/data.js
+++ js/data.js
@@ -1,31 +1,39 @@
 class Data {
   //get
   async get(url) {
-    const response = await fetch(`https://simple-bonds.ajmurphy.co.uk/${url}`),
+    const response = await fetch(
+        `https://my-json-server.typicode.com/aj-murphy/simple-bonds-api/${url}`
+      ),
       responseData = await response.json();
     return responseData;
   }
 
   //post
   async post(url, data) {
-    const response = await fetch(`http://localhost:3000/${url}`, {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json"
-        },
-        body: JSON.stringify(data)
-      }),
+    const response = await fetch(
+        `https://my-json-server.typicode.com/aj-murphy/simple-bonds-api/${url}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify(data)
+        }
+      ),
       responseData = await response.json();
     return responseData;
   }
 
   async delete(url) {
-    const response = await fetch(`http://localhost:3000/${url}`, {
-      method: "DELETE",
-      headers: {
-        "content-type": "application/json"
+    const response = await fetch(
+      `https://my-json-server.typicode.com/aj-murphy/simple-bonds-api/${url}`,
+      {
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json"
+        }
       }
-    });
+    );
 
     responseData = await response.json();
     return responseData;
